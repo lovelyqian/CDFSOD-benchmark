39e230eecb10fa26bffa2a94f3702933467aab15
AImind <13302010070@fudan.edu.cn>
2024-07-14 12:32:22 +0800

    modify scripts

--- main_results.sh
+++ main_results.sh
@@ -20,7 +20,7 @@ model_list=(
 for model in "${model_list[@]}"; do
   for dataset in "${datalist[@]}"; do
     for shot in "${shot_list[@]}"; do
-      CUDA_VISIBLE_DEVICES=2,5,6,7 python tools/train_net.py --num-gpus 4 --config-file configs/${dataset}/vit${model}_shot${shot}_${dataset}_finetune.yaml MODEL.WEIGHTS weights/trained/few-shot/vit${model}_0089999.pth DE.OFFLINE_RPN_CONFIG configs/RPN/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml OUTPUT_DIR output/opensource_main_check/vit${model}_shot${shot}_${dataset}/
+      CUDA_VISIBLE_DEVICES=2,5,6,7 python tools/train_net.py --num-gpus 4 --config-file configs/${dataset}/vit${model}_shot${shot}_${dataset}_finetune.yaml MODEL.WEIGHTS weights/trained/few-shot/vit${model}_0089999.pth DE.OFFLINE_RPN_CONFIG configs/RPN/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml OUTPUT_DIR output/vit${model}/${dataset}_${shot}shot/
     done
   done
 done
