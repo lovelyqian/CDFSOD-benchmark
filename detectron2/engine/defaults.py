2a9f5df8d313b46204113c14e18ad90af5f9784c
AImind <58540231+AImind@users.noreply.github.com>
2024-07-25 21:46:21 +0800

    Update defaults.py

--- detectron2/engine/defaults.py
+++ detectron2/engine/defaults.py
@@ -118,7 +118,7 @@ Run on multiple machines:
     )
     parser.add_argument("--eval-only", action="store_true", help="perform evaluation only")
     parser.add_argument("--num-gpus", type=int, default=1, help="number of gpus *per machine*")
-    parser.add_argument("--controller", type=bool, default=False, help="input True when FT-2heads-baseline")
+    parser.add_argument("--controller", action="store_true", help="add when FT-2heads-baseline")
     parser.add_argument("--num-machines", type=int, default=1, help="total number of machines")
     parser.add_argument(
         "--machine-rank", type=int, default=0, help="the rank of this machine (unique per machine)"
